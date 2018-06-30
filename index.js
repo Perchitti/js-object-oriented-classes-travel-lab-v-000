50ae7d0963225207e68427df1b9733ce0d036601
Lauren Perchitti <laurenperchitti@gmail.com>
2018-06-30 17:39:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 class Driver {
   constructor(name, startDate){
   this.name = name
-  this.startDate = new Date
+  this.startDate = new Date(startDate)
   }
   yearsExperienceFromBeginningOf(){
     return(endDate.getFullYear - this.startDate)
